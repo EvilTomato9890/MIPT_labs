d3d3b530e440a393651bfedf35113424a2de3679
EvilTomato9890 <omglol9898719@gmail.com>
2026-03-07 19:29:18 +0300

    Init structure

---
+++ lab1/common/include/asserts.h
@@ -0,0 +1,55 @@
+#ifndef ASSERTS_H_INCLUDED
+#define ASSERTS_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "colors.h"
+
+#define stringify(a) #a
+
+#ifdef NDEBUG
+    #define HARD_ASSERT(...) (void)0
+#else
+    #define HARD_ASSERT(test, message)                                                   \
+        do {                                                                             \
+            if (!(test)) {                                                               \
+                fprintf(stderr,                                                          \
+                        RED "%s\nERROR OCCURRED IN %s:%d (%s) BY %s\n" RESET,           \
+                        (message),                                                       \
+                        __FILE__,                                                        \
+                        __LINE__,                                                        \
+                        __func__,                                                        \
+                        stringify(test));                                                \
+                abort();                                                                 \
+            }                                                                            \
+        } while (0)
+#endif
+
+#ifdef NDEBUG
+    #define SOFT_ASSERT_FUNCTIONAL(test, message, command)                                 \
+        do {                                                                                \
+            if (!(test)) {                                                                  \
+                command;                                                                    \
+            }                                                                               \
+        } while (0)
+#else
+    #define SOFT_ASSERT_FUNCTIONAL(test, message, command)                               \
+        do {                                                                             \
+            if (!(test)) {                                                               \
+                fprintf(stderr,                                                          \
+                        RED "%s\nERROR OCCURRED IN %s:%d (%s) BY %s\n" RESET,           \
+                        (message),                                                       \
+                        __FILE__,                                                        \
+                        __LINE__,                                                        \
+                        __func__,                                                        \
+                        stringify(test));                                                \
+                command;                                                                 \
+            }                                                                            \
+        } while (0)
+#endif
+
+#define SOFT_ASSERT(test, message) SOFT_ASSERT_FUNCTIONAL((test), (message), (void)0)
+#define SOFT_ASSERT_FUCTIONAL(test, message, command) SOFT_ASSERT_FUNCTIONAL((test), (message), (command))
+
+#endif
---
+++ lab1/common/include/logger.h
@@ -0,0 +1,31 @@
+#ifndef LAB1_LOGGER_H_NCLUDED
+#define LAB1_LOGGER_H_NCLUDED
+
+#include <stdio.h>
+
+typedef enum logger_mode_type {
+    LOGGER_MODE_DEBUG = 0,
+    LOGGER_MODE_INFO = 1,
+    LOGGER_MODE_WARNING = 2,
+    LOGGER_MODE_ERROR = 3
+} logger_mode_type;
+
+void logger_initialize_stream(FILE *stream);
+int logger_initialize_file(const char *path);
+void logger_close(void);
+
+void logger_log_message(logger_mode_type mode, const char *file, int line, const char *format, ...);
+
+#ifdef LOGGER_ALL
+    #define LOGGER_DEBUG(...) logger_log_message(LOGGER_MODE_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+    #define LOGGER_INFO(...) logger_log_message(LOGGER_MODE_INFO, __FILE__, __LINE__, __VA_ARGS__)
+    #define LOGGER_WARNING(...) logger_log_message(LOGGER_MODE_WARNING, __FILE__, __LINE__, __VA_ARGS__)
+#else
+    #define LOGGER_DEBUG(...) (void)0
+    #define LOGGER_INFO(...) (void)0
+    #define LOGGER_WARNING(...) (void)0
+#endif
+
+#define LOGGER_ERROR(...) logger_log_message(LOGGER_MODE_ERROR, __FILE__, __LINE__, __VA_ARGS__)
+
+#endif /* LAB1_LOGGER_H_NCLUDED */
